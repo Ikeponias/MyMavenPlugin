719ad872bed915e0f0bf32c1f672367e4b5288a6
ikeponias <sephiroth929@gmail.com>
2018-10-29 21:16:44 +0900

    Progress Bar Plugin

---
+++ src/main/java/jp/ikeponias/ProgressMojo.java
@@ -0,0 +1,49 @@
+package jp.ikeponias;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "progress")
+public class ProgressMojo extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+		// getLog().info("Hello World");
+		for (int i = 0; i < 100; i++) {
+			System.out.print(String.format("\r[", i));
+			for (int j = 0; j < 10; j++) {
+				if (j < i / 10)
+					System.out.print(String.format("*"));
+				else
+					System.out.print(String.format("-"));
+			}
+
+			System.out.print(String.format("]"));
+			System.out.print(String.format("%d %%", i));
+			try {
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println();
+	}
+}
